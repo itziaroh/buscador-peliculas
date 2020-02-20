095bad164c84c4750da1e455500519404354f199
Itziar Ortega <itziarortega94@gmail.com>
2020-02-20 13:38:01 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import 'bulma/css/bulma.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        BUSCADOR DE PELICULAS
+      </div>
+    );
+  }
+}
+
+export default App;
