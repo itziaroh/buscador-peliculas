611becd10865d6be5ad8dfc0f913b9bff44a684f
Itziar Ortega <itziarortega94@gmail.com>
2020-06-09 19:01:28 +0200

    Add fetch movies with redux pattern. Create docs folder

---
+++ docs/precache-manifest.d4335918786baefcd3c8810214a493b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f340405f2cbc388895951a00f34577b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f644faa1a1f030fa7abd",
+    "url": "/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "7ed7a83f15f1acaa3d16",
+    "url": "/static/css/main.9db73316.chunk.css"
+  },
+  {
+    "revision": "f644faa1a1f030fa7abd",
+    "url": "/static/js/2.0d3a1b7e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0d3a1b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed7a83f15f1acaa3d16",
+    "url": "/static/js/main.3a119dfc.chunk.js"
+  },
+  {
+    "revision": "b8a9c521046294b16ae1",
+    "url": "/static/js/runtime-main.18418bbf.js"
+  }
+]);
--- src/components/ButtonBackHome.js
+++ src/components/ButtonBackHome.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ButtonBackHome = () => {
-    return (
-        <Link
-            to='/'
-            className="button is-warning btn-back"
-        >
-            Back to Home
-        </Link>
-    )
+	return (
+		<Link
+			to='/'
+			className="button is-warning btn-back"
+		>
+			Back to Home
+		</Link>
+	)
 }
 
 export default ButtonBackHome;
