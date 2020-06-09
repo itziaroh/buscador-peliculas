25e9b1aa0a96a41ecd076d73c03613bef9dcb221
Itziar Ortega <itziarortega94@gmail.com>
2020-06-09 19:12:57 +0200

    Change http for https. Add docs folder

---
+++ docs/precache-manifest.b418605ac2adb332ccb679b82794672d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaca415a9031e6b50a772942d6452fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f644faa1a1f030fa7abd",
+    "url": "./static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "3ca83d72b94f1b95f6ff",
+    "url": "./static/css/main.9db73316.chunk.css"
+  },
+  {
+    "revision": "f644faa1a1f030fa7abd",
+    "url": "./static/js/2.0d3a1b7e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.0d3a1b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca83d72b94f1b95f6ff",
+    "url": "./static/js/main.ce7dd187.chunk.js"
+  },
+  {
+    "revision": "6a6f1bb58ddf4546f799",
+    "url": "./static/js/runtime-main.0a6afb99.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4335918786baefcd3c8810214a493b5.js"
+  "./precache-manifest.b418605ac2adb332ccb679b82794672d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
